d68496021b7016138252a114f87adc82721730ef
김승우 <ksw64285@naver.com>
2023-08-19 17:11:18 +0900

    Feat : 헤더 개발

--- youtube/src/App.js
+++ youtube/src/App.js
@@ -1,7 +1,9 @@
 import { Outlet } from "react-router-dom";
+import SearchHeader from "./components/SearchHeader";
 function App() {
   return (
     <>
+      <SearchHeader />
       <Outlet />
     </>
   );
--- youtube/src/pages/Videos.jsx
+++ youtube/src/pages/Videos.jsx
@@ -1,5 +1,7 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 export default function NotFound() {
-  return <div>Videos</div>;
+  const { keyword } = useParams();
+  return <div>Videos {keyword ? `${keyword} 검색` : "핫트렌드"}</div>;
 }
