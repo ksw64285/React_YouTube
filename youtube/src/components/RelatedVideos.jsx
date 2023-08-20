630c6140ce8ec3a5e0c145ffecbf6aa494f40f85
김승우 <ksw64285@naver.com>
2023-08-20 15:21:27 +0900

    Feat : 연관 동영상 부분 (랜덤)

--- youtube/src/components/RelatedVideos.jsx
+++ youtube/src/components/RelatedVideos.jsx
@@ -1,24 +1,22 @@
-import { useQuery } from "@tanstack/react-query";
 import React from "react";
-import { useParams } from "react-router-dom";
-import VideoCard from "../components/VideoCard";
 import { useYoutubeApi } from "../context/YoutubeApiContext";
+import { useQuery } from "@tanstack/react-query";
+import VideoCard from "./VideoCard";
 
-export default function Videos() {
-  const { keyword } = useParams();
+export default function RelatedVideos({ id }) {
   const { youtube } = useYoutubeApi();
   const {
     isLoading,
     error,
     data: videos,
-  } = useQuery(["videos", keyword], () => youtube.search(keyword));
+  } = useQuery(["related", id], () => youtube.relatedVideos(id));
 
   return (
     <>
       {isLoading && <p>로딩중</p>}
       {error && <p>에러 발생</p>}
       {videos && (
-        <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-2 gap-y-4">
+        <ul>
           {videos.map((video) => (
             <VideoCard key={video.id} video={video} />
           ))}
